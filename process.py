aa68bd02d9d8b50a55277248bd3d69f814fd6a9d
YukariChiba <i@0x7f.cc>
2022-04-06 20:04:07 +0800

    Add dress-lite

--- process.py
+++ process.py
@@ -1,6 +1,7 @@
 import json
 
 d = {}
+dl = {}
 
 def loop_dir(directory, base):
     arr = []
@@ -16,13 +17,23 @@ def loop_dir(directory, base):
 
 with open("tmp/tmp.json") as rf:
     j = json.load(rf)
+with open("tmp/tmp-lite.json") as rf:
+    jl = json.load(rf)
 
 for folder in j:
     if folder["type"] == "directory":
         arr = loop_dir(folder, "")
         if len(arr) != 0:
-            d[folder["name"]] = loop_dir(folder, "")
+            d[folder["name"]] = arr
+
+for folder in jl:
+    if folder["type"] == "directory":
+        arrl = loop_dir(folder, "")
+        if len(arrl) != 0:
+            dl[folder["name"]] = arrl
 
 with open("data/dress.json", "w") as wf:
     json.dump(d, wf)
 
+with open("data/dress.lite.json", "w") as wfl:
+    json.dump(dl, wfl)
--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ mkdir -p data
 mkdir -p tmp
 
 tree -J -I '*.md|*.yml' dress | jq '.[0]["contents"]' > tmp/tmp.json
+tree -J -I '*.md|*.yml' dress-lite | jq '.[0]["contents"]' > tmp/tmp-lite.json
 
 python3 process.py
